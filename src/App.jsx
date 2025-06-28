c1c77f55ecc82b6f9f44c2ba2ca86dc3cd061329
amitkvcode <amitkv180@gmail.com>
2025-06-28 15:28:02 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,20 @@ import "./App.css";
 
 const App = () => {
   return (
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <main>
+      <div className="pattern"  />
+
+      <div className="wrapper">
+        <header>
+          <h1>
+            <img src="./hero.png" alt="Hero Banner" />
+            Find <span className="text-gradient">Movies</span> You’ll Love Without the Hassle
+          </h1>
+        </header>
+      </div>
+  
+  </main>
   );
-}
+};
 
 export default App;
