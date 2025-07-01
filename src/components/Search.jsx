482d88ee2d3a1c891c222d677f181b4f11bba1f4
amitkvCode <amitkv180@gmail.com>
2025-07-01 19:32:14 +0530

    Initial commit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const Search = ({searchTerm , setsearchTerm}) => {
+const Search = ({ searchTerm, setsearchTerm }) => {
   return (
     <div className="search">
       <div>
         <img src="search.png" alt="Search" />
-        <input 
-        type="text"
-        placeholder='Serach thoungh thousands of movies'
-        value={searchTerm}
-        onChange={(event)=> setsearchTerm(event.target.value)}
+        <input
+          type="text"
+          placeholder="Serach thoungh thousands of movies"
+          value={searchTerm} // You use value={searchTerm} to bind the input box to the React state.
+          // React doesn't know how to "connect" the input box to searchTerm unless you explicitly tell it using value={searchTerm}.
+          onChange={(event) => setsearchTerm(event.target.value)} // event is an object, and it contains a nested object called target, and that object has a property called value.
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
