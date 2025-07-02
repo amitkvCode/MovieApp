776f01efd857cedd6f93da15c99f92be5fdfab58
amitkvCode <amitkv180@gmail.com>
2025-07-02 13:18:12 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,24 @@ import { useState } from "react";
 import "./App.css";
 import Search from "./components/Search";
 
-
 const App = () => {
-  const [searchTerm, setsearchTerm] = useState('')
+  const [searchTerm, setsearchTerm] = useState(""); // Ues for Search Movies
   return (
     <main>
-      <div className="pattern"  />
+      <div className="pattern" />
 
       <div className="wrapper">
         <header>
           <h1>
             <img src="./hero.png" alt="Hero Banner" />
-            Find <span className="text-gradient">Movies</span> You’ll Love Without the Hassle
+            Find <span className="text-gradient">Movies</span> You’ll Love
+            Without the Hassle
           </h1>
         </header>
 
         <Search searchTerm={searchTerm} setsearchTerm={setsearchTerm}></Search>
       </div>
-  
-  </main>
+    </main>
   );
 };
 
